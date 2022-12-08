303d00ef7d70fde746e6f8983083668ebaf5f379
aibianse <119503284+aibianse@users.noreply.github.com>
2022-12-08 20:45:31 +0800

    Update python_pixiv.py

--- python_pixiv.py
+++ python_pixiv.py
@@ -1,3 +1,38 @@
+# import requests
+# import re
+#
+# headers = {
+#     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
+#     'referer': 'https://www.pixiv.net/ranking.php?mode=male&content=illust',
+# }
+#
+# path = './pixiv_images/'
+# repeat = 1
+# count = 1
+# for n in range(1, 2):
+#     url = 'https://www.pixiv.net/ranking.php?mode=male&content=illust&p=%d&format=json' % n
+#     response = requests.get(url, headers=headers)
+#     illust_id = re.findall('"illust_id":(\d+?),', response.text)
+#     picUrl = ['https://www.pixiv.net/artworks/' + i for i in illust_id]
+#     for url in picUrl:
+#         print('正在下载第 %d 张图片' % count, end='   ')
+#         response = requests.get(url, headers=headers)
+#         # # 提取图片名称
+#         # name = re.search('"illustTitle":"(.+?)"', response.text)
+#         # name = name.group(1)
+#         # illust_id = re.search('"illustId":"(.+?)"', response.text)
+#         # illust_id = illust_id.group(1)
+#         # if re.search('[\\\ \/ \* \? \" \: \< \> \|]', name) != None:
+#         #     name = re.sub('[\\\ \/ \* \? \" \: \< \> \|]', str(repeat), name)
+#         #     repeat += 1
+#         # 提取图片原图地址
+#         picture = re.search('"original":"(.+?)"},"tags"', response.text)
+#         pic = requests.get(picture.group(1), headers=headers)
+#         f = open(path + str(count) + '.jpg', 'wb')
+#         f.write(pic.content)
+#         f.close()
+#         print('下载成功', end='\n')
+#         count += 1
 import requests
 import re
 
@@ -6,31 +41,41 @@ headers = {
     'referer': 'https://www.pixiv.net/ranking.php?mode=male&content=illust',
 }
 
-path = './pixiv_images/'
+path = 'M:/'
 repeat = 1
-count = 1
-for n in range(1, 2):
-    url = 'https://www.pixiv.net/ranking.php?mode=male&content=illust&p=%d&format=json' % n
+
+
+def getSinglePic(url):
+    global repeat
     response = requests.get(url, headers=headers)
-    illust_id = re.findall('"illust_id":(\d+?),', response.text)
-    picUrl = ['https://www.pixiv.net/artworks/' + i for i in illust_id]
-    for url in picUrl:
-        print('正在下载第 %d 张图片' % count, end='   ')
+    # 提取图片名称
+    name = re.search('"illustTitle":"(.+?)"', response.text)
+    name = name.group(1)
+    illust_id = re.search('"illustId":"(.+?)"', response.text)
+    illust_id = illust_id.group(1)
+    if re.search('[\\\ \/ \* \? \" \: \< \> \|]', name) != None:
+        name = re.sub('[\\\ \/ \* \? \" \: \< \> \|]', str(repeat), name)
+        repeat += 1
+    # 提取图片原图地址
+    picture = re.search('"original":"(.+?)"},"tags"', response.text)
+    pic = requests.get(picture.group(1), headers=headers)
+    f = open(path + '%s_%s.%s' % (illust_id, name, picture.group(1)[-3:]), 'wb')
+    f.write(pic.content)
+    f.close()
+
+
+def getAllPicUrl():
+    count = 1
+    for n in range(1, 2):
+        url = 'https://www.pixiv.net/ranking.php?mode=male&content=illust&p=%d&format=json' % n
         response = requests.get(url, headers=headers)
-        # # 提取图片名称
-        # name = re.search('"illustTitle":"(.+?)"', response.text)
-        # name = name.group(1)
-        # illust_id = re.search('"illustId":"(.+?)"', response.text)
-        # illust_id = illust_id.group(1)
-        # if re.search('[\\\ \/ \* \? \" \: \< \> \|]', name) != None:
-        #     name = re.sub('[\\\ \/ \* \? \" \: \< \> \|]', str(repeat), name)
-        #     repeat += 1
-        # 提取图片原图地址
-        picture = re.search('"original":"(.+?)"},"tags"', response.text)
-        pic = requests.get(picture.group(1), headers=headers)
-        f = open(path + str(count) + '.jpg', 'wb')
-        f.write(pic.content)
-        f.close()
-        print('下载成功', end='\n')
-        count += 1
-        
+        illust_id = re.findall('"illust_id":(\d+?),', response.text)
+        picUrl = ['https://www.pixiv.net/artworks/' + i for i in illust_id]
+        for url in picUrl:
+            print('正在下载第 %d 张图片' % count, end='   ')
+            getSinglePic(url)
+            print('下载成功', end='\n')
+            count += 1
+    return None
+
+getAllPicUrl()
