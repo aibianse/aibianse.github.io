a640517e1f4e1ef628e70123bdc942edb4493894
aibianse <119503284+aibianse@users.noreply.github.com>
2023-01-01 15:06:35 +0800

    Update xhentai.py

--- xhentai.py
+++ xhentai.py
@@ -22,7 +22,8 @@ if response.status_code == 200:
     print(response_code)
     if response_code == 206 or response_code == 200:
         data_json = {"video_url": url}
-        with open('data.json', 'w') as f:
+        file_path = './pixiv_images/data.json'
+        with open(file_path, 'w') as f:
             json.dump(data_json, f)
             print('写入成功')
 
