c6d485d04af74ab6e462647d13b11cae4b2209d9
aibianse <119503284+aibianse@users.noreply.github.com>
2023-02-19 13:53:22 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,9 +1,19 @@
-function pixiv_images() {
+function pixiv_hentai_illust() {
     var img_html = '';
     for (var i = 1; i <= 50; i++) {
         img_html += '<img class="nya-btn hot badge" src="./pixiv/pixiv_data/hentai_illust/aibianse_com_' + i + '.webp" referrerpolicy="no-referrer" alt="爱变色Pixiv Hentai Illust"/></br>';
     }
-    $('#pixiv_images').html(img_html);
+    $('#pixiv_hentai_illust').html(img_html);
+}
+function pixiv_hentai_manga() {
+    $.get("https://aibianse.github.io/pixiv/pixiv_data/data.json", function (data) {
+        var urls = data.urls
+        var img_html = '';
+        for (var i = 0; i <= urls.length; i++) {
+            img_html += '<img class="nya-btn hot badge" src="' + urls[i] + '" referrerpolicy="no-referrer" alt="爱变色Pixiv Hentai Manga"/></br>';
+        }
+        $('#pixiv_hentai_manga').html(img_html);
+    }, "json");
 }
 function xhentai_video() {
     $.get("https://aibianse.github.io/file/xhentai_data.json", function (data) {
